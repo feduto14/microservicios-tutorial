a756bb9d090931f60dadd5c62650588bbf57e790
Federico <federico.l.cross@gmail.com>
2023-04-08 04:13:08 -0300

    update gateway cfg with load balancer

---
+++ gateway-service/src/test/java/ar/com/gateway/service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
